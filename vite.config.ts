7b6ffbdffe581bd899d51268a03e39aa14f095c6
Mugasira Calvin <152085655+Mugasir@users.noreply.github.com>
2026-05-07 14:21:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import { TanStackRouterPlugin } from '@tanstack/router-plugin'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      target: 'cloudflare',
-    }),
+    tanstackStart({ target: 'cloudflare' }),
+    TanStackRouterPlugin(),
+    react(),
+    cloudflare(),
   ],
 })
